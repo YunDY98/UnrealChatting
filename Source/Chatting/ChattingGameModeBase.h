a5a704947d8bc267d6e0ebfda244d88688868edf
yun <dbsejrdid123@naver.com>
2022-12-12 11:28:24 +0900

    chatting system

---
+++ Source/Chatting/ChattingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ChattingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHATTING_API AChattingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
